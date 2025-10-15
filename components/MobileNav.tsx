d687cf0d8c6748363397efed67a5af19519b48b5
Ziyaa Danil M <ziyaadanil37@gmail.com>
2025-10-15 14:24:30 +0700

    add component sidebar

---
+++ components/MobileNav.tsx
@@ -0,0 +1,61 @@
+
+'use client';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+
+const MobileNav = ({user} : MobileNavProps) => {
+    const pathName = usePathname();
+    return (
+        <section className="w-fulll max-w-[264px]">
+            <Sheet>
+                <SheetTrigger>
+                    <Image src="/icons/hamburger.svg" width={30} height={30} alt="menu"/>
+                </SheetTrigger>
+                <SheetContent side="left" className="border-none bg-white">
+                    <SheetHeader>
+                        <Link href="/" className="flex cursor-pointer items-center ">
+                            <Image src="/icons/logo.svg" width={34} height={34} alt="logo" />
+                            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Noxic</h1>
+                        </Link>
+                        <div className="mobilenav-sheet">
+                            <SheetClose asChild>
+                                <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                                    {sidebarLinks.map((item) => {
+                                        const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`);
+                                        return (
+                                            <SheetClose asChild key={item.route}>
+                                                <Link href={item.route} key={item.label} className={cn('mobilenav-sheet_close w-full', {'bg-bank-gradient' : isActive} )} >
+                                                    <Image src={item.imgURL} alt={item.label} width={20} height={20} className={cn({'brightness-[3] invert-0': isActive})} />
+                                                    <p className={cn("text-16 font-semibold text-black-2", {'text-white' : isActive})}>
+                                                        {item.label}
+                                                    </p>
+                                                </Link>
+                                            </SheetClose>
+                                        )
+                                    })}
+
+                                    User
+                                </nav>
+                            </SheetClose>
+
+                            Footer
+                        </div>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet>
+        </section>
+    )
+}
+
+export default MobileNav ;
---
+++ components/Sidebar.tsx
@@ -0,0 +1,41 @@
+'use client';
+import Link from "next/link";
+import  Image  from "next/image";
+import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+const Sidebar = ({
+    user
+}:SiderbarProps) => {
+    const pathName = usePathname();
+
+    return (
+        <section className="sidebar">
+            <nav className="flex flex-col gap-4">
+                <Link href="/" className="flex mb-12 cursor-pointer items-center ">
+                    <Image src="/icons/logo.svg" width={34} height={34} alt="logo" className="size-[24px] max-xl:size-14" />
+                    <h1 className="sidebar-log">Noxic</h1>
+                </Link>
+                {sidebarLinks.map((item) => {
+                    const isActive = pathName === item.route || pathName.startsWith(`${item.route}/`);
+                    return (
+                        <Link href={item.route} key={item.label} className={cn('sidebar-link',{'bg-bank-gradient' : isActive} )} >
+                            <div className="relative size-6">
+                                <Image src={item.imgURL} alt={item.label} fill className={cn({'brightness-[3] invert-0': isActive})} />
+                            </div>
+                            <p className={cn('sidebar-label', {'!text-white' : isActive})}>
+                                {item.label}
+                            </p>
+                        </Link>
+                    )
+                })}
+
+                User
+            </nav>
+                Footer
+        </section>
+    )
+}
+
+export default Sidebar ;
